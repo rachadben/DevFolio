f842a0491781f9fcadefee41147b2040b322775c
rachadben <rachadbenabdelkader@gmail.com>
2024-01-20 12:37:45 +0000

    jspfrere

---
+++ script.js
@@ -0,0 +1,23 @@
+function sendMessage() {
+    (function(){
+        emailjs.init("TfzgLWu6U3hHWVFIm");
+    })();
+    
+    var servieID= "service_umb5x8f";
+    var templateID= "template_hxu1z4n";
+
+    var params = {
+        sendername: document.querySelector("from_name").value,
+        senderemail: document.querySelector("email_id").value,
+        subject: document.querySelector("subject").value,
+        message: document.querySelector("message").value
+    };
+
+    emailjs.send("service_umb5x8f", "template_hxu1z4n", params).then(res => {
+        alert('thank you, '+ params['sendername'] + '! Your message as been sent.');
+     })
+     .catch();
+
+
+}
+
